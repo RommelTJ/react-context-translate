c4b841c9e3835e2c0426bd2b483d1d4af39623f0
Rommel Rico <me@rommelrico.com>
2019-01-31 20:13:00 -0800

    Using a Consumer component to update the Field component

--- src/components/Field.js
+++ src/components/Field.js
@@ -3,14 +3,18 @@ import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
 
-    static contextType = LanguageContext; // this is a particular variable name.
+    // static contextType = LanguageContext; // this is a particular variable name.
 
     render() {
-        const text = this.context === "english" ? "Name" : "Nombre";
+        // const text = this.context === "english" ? "Name" : "Nombre";
 
         return (
             <div className="ui field">
-                <label>{text}</label>
+                <label>
+                    <LanguageContext.Consumer>
+                        {(value) => value === "english" ? "Name" : "Nombre"}
+                    </LanguageContext.Consumer>
+                </label>
                 <input/>
             </div>
         );
