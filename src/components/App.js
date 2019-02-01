8e839b5910db2184ed8fba983ee6383acd36e194
Rommel Rico <me@rommelrico.com>
2019-01-31 20:18:23 -0800

    Adding a second context provider to UserCreate

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
 
@@ -19,7 +20,9 @@ class App extends React.Component {
                     <i className="flag mx" onClick={() => this.onLanguageChange('spanish')}/>
                 </div>
                 <LanguageContext.Provider value={this.state.language}>
-                    <UserCreate/>
+                    <ColorContext.Provider value="blue">
+                        <UserCreate/>
+                    </ColorContext.Provider>
                 </LanguageContext.Provider>
             </div>);
     }
