46eb8a3d30ba83bc76ba0ded4981f6bfbf1dbea1
Rommel Rico <me@rommelrico.com>
2019-01-31 20:08:50 -0800

    Using a Consumer component instead of a context

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,17 @@ import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
 
-    static contextType = LanguageContext; // this is a particular variable name.
+    // static contextType = LanguageContext; // this is a particular variable name.
 
     render() {
-        const text = this.context === "english" ? "Submit" : "Enviar";
-        return <button className="ui button primary">{text}</button>;
+        // const text = this.context === "english" ? "Submit" : "Enviar";
+        return (
+            <button className="ui button primary">
+                <LanguageContext.Consumer>
+                    {(value) => value === "english" ? "Submit": "Enviar"}
+                </LanguageContext.Consumer>
+            </button>
+        );
     }
 }
 
