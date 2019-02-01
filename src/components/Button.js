f5d99b7dcde08d64e8ce4cfad233dd7067c6c65f
Rommel Rico <me@rommelrico.com>
2019-01-31 19:40:42 -0800

    Additional note

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
 
-    static contextType = LanguageContext;
+    static contextType = LanguageContext; // this is a particular variable name.
 
     render() {
         return <button className="ui button primary">Submit</button>;
